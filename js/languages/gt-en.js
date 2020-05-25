52d1e6c8a76e4dbb9ad54eca812e66cdcdcce5d6
MoshaGames <60564173+MoshaGames@users.noreply.github.com>
2020-05-25 17:21:08 -0600

    Add files via upload

---
+++ js/languages/gt-en.js
@@ -0,0 +1,29 @@
+(function($){
+  $.gt_en = {
+	'MIMI_0_0': 'Hi, welcome back <span class="strong">' + $.userName() + '</span>.',
+	'MIMI_0_1': 'Please, use the Menu to select a Minigame.',
+
+	'MIMI_1_0': 'I\'m <span class="strong">Mimi</span>. Nice to meet you.',
+
+	'MIMI_2_0': 'You need <span class="strong">' + ( ( $.user('user_featured') ) ? '5' : '10' ) + ' Fame Points</span> to be able to submit your score.',
+	'MIMI_2_1': 'You will gain Muns equal to the Score you get, as well as Recover <span class="strong">20% of your HP/MP</span>!',
+
+	'MIMI_3_0': 'Want to know a secret? Tehee...',
+	'MIMI_3_1': 'I love intelligent people!',
+	'MIMI_3_2': 'You will impress me if you score <span class="strong">above 9</span>.',
+
+	'MIMI_4_0': 'I can see you are really smart...',
+	'MIMI_4_1': 'I like you!',
+	'MIMI_4_2': 'Want a cup of coffee?',
+
+	'MIMI_5_0': 'You can take a photo and send your score to your friends.',
+	'MIMI_6_0': 'To get an A+ you must get all the answers right.',
+	'MIMI_7_0': 'To get the golden trophy you must get the <span class="strong">Highest Score</span>.',
+
+	'MIMI_LESSON_1': 'Fill in the <span class="strong">MISSING SIGN</span> in the equation as quickly as you can. Are you Ready?',
+	'MIMI_LESSON_1_1': 'Fill in the <span class="strong">MISSING SIGN or NUMBER</span> in the equation as quickly as you can. Are you Ready?',
+	'MIMI_LESSON_2': 'Fill in the <span class="strong">MISSING SIGN</span> in the equation as quickly as you can. Are you Ready?',
+	'MIMI_LESSON_3': 'Fill in the <span class="strong">MISSING NUMBER</span> in the equation as quickly as you can. Are you Ready?',
+
+  };
+})(jQuery);
---
+++ js/languages/gt-es.js
@@ -0,0 +1,29 @@
+(function($){
+  $.gt_es = {
+	'MIMI_0_0': 'Hola, <span class="strong">' + $.userName() + '</span>.',
+	'MIMI_0_1': 'Utiliza el Menú para seleccionar un Mini juego.',
+
+	'MIMI_1_0': 'Hola, soy <span class="strong">Mimi</span>.',
+
+	'MIMI_2_0': 'Necesitas <span class="strong">' + ( ( $.user('user_featured') ) ? '5' : '10' ) + ' Puntos de Fama</span> para enviar tu puntaje.',
+	'MIMI_2_2': 'Además, ¡podras obtener Muns mientras juegas y recuperar <span class="strong">20% de tu HP/MP</span>!',
+
+	'MIMI_3_0': '¿Quiéres saber un secretito? Tehee...',
+	'MIMI_3_1': '¡Me encantan las personas inteligentes!',
+	'MIMI_3_2': 'Me vas a impresionar si obtienes un puntaje <span class="strong">mayor a 9</span>.',
+
+	'MIMI_4_0': 'Puedo ver que eres una persona muy lista...',
+	'MIMI_4_1': '¡Me encantas!',
+	'MIMI_4_2': '¿Quieres un café?',
+
+	'MIMI_5_0': 'Puedes tomar una foto y enviar tu puntaje a todos tus amigos.',
+	'MIMI_6_0': 'Para obtener una Calificación de A+ debes obtener todas las respuestas correctas.',
+	'MIMI_7_0': 'Para obtener el trofeo dorado, necesitas obtener el <span class="strong">Puntaje mas alto</span>.',
+
+
+	'MIMI_LESSON_1': 'Llena el <span class="strong">SIGNO FALTANTE</span> en la operación tan rápido como puedas. ¿Entendido?',
+	'MIMI_LESSON_1_1': 'Llena el <span class="strong">SIGNO o NÚMERO FALTANTE</span> en la operación tan rápido como puedas. ¿Entendido?',
+	'MIMI_LESSON_2': 'Llena el <span class="strong">NÚMERO FALTANTE</span> en la operación tan rápido como puedas. ¿Entendido?',
+
+  };
+})(jQuery);
